ee49239b6d6e73198b550baf1bba5979544c8d86
Ravshanbek <ahmedovravshanbek070@gmail.com>
2023-05-17 18:29:29 +0500

    Category finish

--- src/main/java/com/company/YouTubeProject/controller/CategoryController.java
+++ src/main/java/com/company/YouTubeProject/controller/CategoryController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/category")
 public class CategoryController {
@@ -20,14 +22,23 @@ public class CategoryController {
         return ResponseEntity.ok(service.created(dto));
     }
 
-    @PutMapping("/private/update/adm")
+    @PutMapping("/private/update/adm/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    private ResponseEntity<CategoryDto> update(@RequestBody CategoryDto dto) {
-        return ResponseEntity.ok(service.update(dto));
+    private ResponseEntity<CategoryDto> update(@PathVariable("id") Integer id,@RequestBody CategoryDto dto) {
+        return ResponseEntity.ok(service.update(id,dto));
     }
 
 
+    @DeleteMapping("/private/delete/adm/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    private ResponseEntity<Boolean> delete(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(service.delete(id));
+    }
 
+    @GetMapping("/public/list")
+    private ResponseEntity<List<CategoryDto>> list() {
+        return ResponseEntity.ok(service.getALl());
+    }
 
 
 }
--- src/main/java/com/company/YouTubeProject/service/CategoryService.java
+++ src/main/java/com/company/YouTubeProject/service/CategoryService.java
@@ -3,12 +3,15 @@ package com.company.YouTubeProject.service;
 import com.company.YouTubeProject.dto.categoryDto.CategoryDto;
 import com.company.YouTubeProject.dto.categoryDto.CategoryResponseDTO;
 import com.company.YouTubeProject.entity.CategoryEntity;
+import com.company.YouTubeProject.exeption.AppBadRequestException;
 import com.company.YouTubeProject.exeption.ItemAlreadyExistsException;
 import com.company.YouTubeProject.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -42,8 +45,41 @@ public class CategoryService {
     }
 
 
-    public CategoryDto update(CategoryDto dto) {
+    public CategoryDto update(Integer id, CategoryDto dto) {
+        Optional<CategoryEntity> optional = categoryRepository.findById(id);
 
-        return null;
+        if (optional.isEmpty()) {
+            throw new AppBadRequestException("yo'qku");
+        }
+        CategoryEntity entity = new CategoryEntity();
+        entity.setName(dto.getName());
+        entity.setCreatedDate(LocalDateTime.now());
+        categoryRepository.save(entity);
+
+        return dto;
+    }
+
+    public boolean delete(Integer id) {
+        Optional<CategoryEntity> optional = categoryRepository.findById(id);
+        if (optional.isEmpty()){
+            throw new AppBadRequestException("bu foydalanuvchi yo'q ");
+        }
+        categoryRepository.delete(optional.get());
+        return true;
+    }
+
+    public List<CategoryDto> getALl() {
+        List<CategoryEntity> entityList = (List<CategoryEntity>) categoryRepository.findAll();
+        List<CategoryDto> dtoList = new LinkedList<>();
+        entityList.forEach(entity ->
+                dtoList.add(toArticleShortInfo(entity)));
+        return dtoList;
+    }
+
+    private CategoryDto toArticleShortInfo(CategoryEntity entity) {
+        CategoryDto dto = new CategoryDto();
+        dto.setId(entity.getId());
+        dto.setName(entity.getName());
+        return dto;
     }
 }
