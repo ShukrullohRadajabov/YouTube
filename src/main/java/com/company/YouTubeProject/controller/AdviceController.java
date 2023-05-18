2e016b15d47b4be165056c3cc9c1abf4d39251ad
Mamurjon <uzokov29@gmail.com>
2023-05-18 21:22:10 +0500

    Commit

---
+++ src/main/java/com/company/YouTubeProject/controller/AdviceController.java
@@ -0,0 +1,23 @@
+package com.company.YouTubeProject.controller;
+
+
+import com.company.YouTubeProject.exeption.AppBadRequestException;
+import com.company.YouTubeProject.exeption.ItemNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.MethodNotAllowedException;
+
+@ControllerAdvice
+public class AdviceController {
+    @ExceptionHandler({AppBadRequestException.class, ItemNotFoundException.class, MethodNotAllowedException.class})
+    public ResponseEntity<String> handleException(RuntimeException e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<?> handleNotValidException(RuntimeException e) {
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+}
