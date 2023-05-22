d4e440eb92f5de59da6a641d98a0b097f1fead6f
Oyatillo <oyatillo.soliyev03@gmail.com>
2023-05-22 15:41:01 +0900

    start tagvideo

---
+++ src/main/java/com/company/YouTubeProject/config/OpenAPiConfig.java
@@ -0,0 +1,38 @@
+package com.company.YouTubeProject.config;
+
+import io.swagger.models.Contact;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+public class OpenAPiConfig {
+    @Value("${server.host}")
+    private String url;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(url);
+        devServer.setDescription("Server URL");
+
+        Contact contact = new Contact();
+        contact.setEmail("kun.uz");
+        contact.setName("BezKoder");
+        contact.setUrl("https://www.bezkoder.com");
+
+
+        Info info = new Info()
+                .title("Kun uz Management API")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes endpoints to manage tutorials.")
+                .termsOfService("https://www.bezkoder.com/terms")
+                .license(null);
+
+        return new OpenAPI().info(info).servers(List.of(devServer));
+    }
+}
--- src/main/java/com/company/YouTubeProject/repository/VideoLikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.YouTubeProject.repository;
-
-import com.company.YouTubeProject.entity.TagEntity;
-import com.company.YouTubeProject.entity.VideoLikeEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface VideoLikeRepository extends CrudRepository<VideoLikeEntity, Integer>, PagingAndSortingRepository<VideoLikeEntity, Integer> {
-}
